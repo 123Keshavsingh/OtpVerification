000317b9c00b1c089021151490dcc5006c35f3b9
keshav singh <keshav.singh@wobot.ai>
2021-09-12 18:24:52 -0700

    updated SuccessScreen

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = ({  }) => {
     <div className="wrapper">
       <h1 className="main-heading">Welcome to AdmitKard</h1>
       <p className="sub-text1">in ordor to provide you with a costum experience</p>
-      <p className="sub-text">we need to ask you a few question</p>
+      <p className="sub-text">we need to ask you a few questions</p>
       <button className="main-button" type="submit" id="sign-in-button">GET STARTED</button>
       <p className="sub-text1">this will take 5 minutes</p>
 
