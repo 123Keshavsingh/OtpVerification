d618782fcbabd32d2d3a7066e5621805fda87321
keshav singh <keshav.singh@wobot.ai>
2021-12-12 16:22:34 +0530

    changed name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,18 @@
 import React from "react";
 
-const Home = ({  }) => {
+const Home = ({}) => {
   //console.log(user);
   return (
     <div className="wrapper">
-      <h1 className="main-heading">Welcome to AdmitKard</h1>
-      <p className="sub-text1">in ordor to provide you with a costum experience</p>
+      <h1 className="main-heading">Welcome to GurujiAstro</h1>
+      <p className="sub-text1">
+        in ordor to provide you with a costum experience
+      </p>
       <p className="sub-text">we need to ask you a few questions</p>
-      <button className="main-button" type="submit" id="sign-in-button">GET STARTED</button>
+      <button className="main-button" type="submit" id="sign-in-button">
+        GET STARTED
+      </button>
       <p className="sub-text1">this will take 5 minutes</p>
-
-      
-      
-
-     
     </div>
   );
 };
